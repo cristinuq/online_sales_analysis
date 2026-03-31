2f53511506ea10b84997043a2a985b25e0111963
Cristiana-Raluca Dima <cristianaralucadima@gmail.com>
2026-03-31 20:32:20 +0300

    Add Cart class and update main with cart functionality

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from product import Product
 from product_manager import ProductManager
-
+from cart import Cart
 manager = ProductManager()
 
 manager.add_product(Product("Laptop", 3500, 10))
 manager.add_product(Product("Mouse", 150, 50))
 manager.add_product(Product("Tastatura", 300, 30))
 
-manager.display_products()
-manager.total_inventory_value()
+cart = Cart()
+cart.add_product(manager.products[0])
+cart.add_product(manager.products[1])
+cart.add_product(manager.products[2])
+
+cart.display_cart()
+cart.total_cart_value()
