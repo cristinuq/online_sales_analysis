649f7d38deb61cbc5ce403d1cf84a25adaa810a8
Cristiana-Raluca Dima <cristianaralucadima@gmail.com>
2026-03-31 20:35:27 +0300

    Update product names and quantities in main

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ from product_manager import ProductManager
 
 manager = ProductManager()
 
-manager.add_product(Product("Laptop", 3500, 10))
-manager.add_product(Product("Mouse", 150, 50))
-manager.add_product(Product("Tastatura", 300, 30))
+manager.add_product(Product("Laptop Ultra", 3500, 5))
+manager.add_product(Product("Mouse Bluetooth", 200, 40))
+manager.add_product(Product("Tastatura Gaming", 500, 15))
 
-manager.display_products()
-manager.total_inventory_value()
